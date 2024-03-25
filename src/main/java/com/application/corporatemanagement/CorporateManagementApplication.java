969fbe53dbf715bbc95b5f61c5fce6dc98bab33f
boundlesssea <xuzefeng159@163.com>
2024-03-25 18:36:23 +0800

    init

---
+++ src/main/java/com/application/corporatemanagement/CorporateManagementApplication.java
@@ -0,0 +1,12 @@
+package com.application.corporatemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorporateManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorporateManagementApplication.class, args);
+    }
+}
---
+++ src/test/java/com/application/corporatemanagement/CorporateManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.corporatemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorporateManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
