8113571bd66125fb24cd83a2b0c94163edf5643d
boundlesssea <xuzefeng159@163.com>
2024-04-18 22:24:51 +0800

    delete useless code

--- src/main/java/com/application/corporatemanagement/domain/orgmng/OrgValidator.java
+++ src/main/java/com/application/corporatemanagement/domain/orgmng/OrgValidator.java
@@ -2,7 +2,6 @@ package com.application.corporatemanagement.domain.orgmng;
 
 import com.application.corporatemanagement.application.orgmng.OrgDto;
 import com.application.corporatemanagement.domain.common.exceptions.BusinessException;
-import com.application.corporatemanagement.domain.common.exceptions.DirtyDataException;
 import com.application.corporatemanagement.domain.common.validator.TenantValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -31,8 +30,6 @@ public class OrgValidator {
     public void validate(OrgDto request) {
         tenantValidator.tenantShouldBeValid(request.getTenant());
         orgTypeValidator.validate(request.getTenant(), request.getOrgType());
-
-//        superiorCheck(request.getTenant(), request.getOrgType(), request.getSuperior());
         superiorValidator.validate(request.getTenant(), request.getSuperior(), request.getOrgType());
 
         corporateShouldNotBeCreatdAlone(request.getOrgType());
@@ -44,38 +41,6 @@ public class OrgValidator {
     private static void corporateShouldNotBeCreatdAlone(String orgType) {
         if (orgType.equals("ENTP")) {
             throw new BusinessException("企业是在创建租户的时候创建好的，因此不能单独创建企业!");
-        }
-    }
-
-
-    private void superiorCheck(Long tenant, String orgType, Long superior) {
-        Org superiorOrg = superiorShouldBeEffective(tenant, superior);
-        OrgType superiorOrgType = superiorOrgTypeShouldBeValid(tenant, superior, superiorOrg.getOrgType());
-        superiorOfDevGroupShouldBeDevCenter(orgType, superiorOrgType.getCode(), superior);
-        superiorOfDevCenterAndDirectDeptShouldBeEntp(orgType, superiorOrgType.getCode(), superior);
-    }
-
-    private Org superiorShouldBeEffective(Long tenant, Long superior) {
-        return orgRepository
-                .findByIdAndStatus(tenant, superior, OrgStatus.EFFECTIVE)
-                .orElseThrow(() -> new BusinessException("'" + superior + "' 不是有效的组织 id !"));
-    }
-
-    private OrgType superiorOrgTypeShouldBeValid(Long tenant, Long superior, String superiorOrgType) {
-        return orgTypeRepository
-                .findByCodeAndStatus(tenant, superiorOrgType, OrgTypeStatus.EFFECTIVE)
-                .orElseThrow(() -> new DirtyDataException("id 为 '" + superior + "' 的组织的组织类型代码 '" + superiorOrgType + "' 无效!"));
-    }
-
-    private static void superiorOfDevGroupShouldBeDevCenter(String orgType, String superiorOrgTypeCode, Long superior) {
-        if ("DEVGRP".equals(orgType) && !"DEVCENT".equals(superiorOrgTypeCode)) {
-            throw new BusinessException("开发组的上级(id = '" + superior + "')不是开发中心！");
-        }
-    }
-
-    private static void superiorOfDevCenterAndDirectDeptShouldBeEntp(String orgType, String superiorOrgTypeCode, Long superior) {
-        if (("DEVCENT".equals(orgType) || "DIRDEP".equals(orgType)) && !"ENTP".equals(superiorOrgTypeCode)) {
-            throw new BusinessException("开发中心或直属部门的上级(id = '" + superior + "')不是企业！");
         }
     }
 
