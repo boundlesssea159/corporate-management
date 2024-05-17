bcab0d3f41dc9956b6a497dd625fdb3d0f9ce5d4
boundlesssea <xuzefeng159@163.com>
2024-05-17 10:25:39 +0800

    simplify test code

--- src/test/java/com/application/corporatemanagement/application/orgmng/emp/EmpServiceTest.java
+++ src/test/java/com/application/corporatemanagement/application/orgmng/emp/EmpServiceTest.java
@@ -1,26 +1,11 @@
 package com.application.corporatemanagement.application.orgmng.emp;
 
-import com.application.corporatemanagement.application.orgmng.emp.AddEmpRequest;
-import com.application.corporatemanagement.application.orgmng.emp.EmpService;
 import com.application.corporatemanagement.domain.orgmng.emp.*;
-import lombok.Data;
-import lombok.Getter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Arrays;
-import java.util.EnumMap;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -39,10 +24,10 @@ class EmpServiceTest {
 
     @Test
     void should_add_emp() {
-        Emp emp = Emp.builder().build();
-        when(empAssembler.fromCreateRequest(any())).thenReturn(emp);
-        empService.addEmp(AddEmpRequest.builder().build(), 10010L);
-        verify(empRepository).save(emp, 10010L);
+        AddEmpRequest request = AddEmpRequest.builder().build();
+        empService.addEmp(request, 10010L);
+        verify(empAssembler).fromCreateRequest(request);
+        verify(empRepository).save(any(),anyLong());
     }
 
     // todo update emp
