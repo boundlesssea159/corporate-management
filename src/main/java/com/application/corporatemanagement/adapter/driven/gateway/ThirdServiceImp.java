90a15c3fa1fe7b55446f7e1bcb5dd9369e1c3ece
boundlesssea <xuzefeng159@163.com>
2024-06-30 15:48:16 +0800

    .

---
+++ src/main/java/com/application/corporatemanagement/adapter/driven/gateway/ThirdServiceImp.java
@@ -0,0 +1,10 @@
+package com.application.corporatemanagement.adapter.driven.gateway;
+
+public class ThirdServiceImp {
+
+    // get Emp from orgmng context
+    // note : the Emp is not the one from orgmng, but is the one from self context
+//    public Emp findById(Long id) {
+//        return null;
+//    }
+}
